0b9b6e5ee90c43da5b3c788800c0a7a79b124744
seventy_eight <41247659+Yongchan1760@users.noreply.github.com>
2020-10-25 23:46:58 +0900

    ..

---
+++ AL_test/src/al_test1025/Linkedlist_4.java
@@ -0,0 +1,24 @@
+package al_test1025;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+class Tiger{
+	int n;
+	String s;
+	public Tiger(int n, String s) {
+		this.n = n;
+		this.s = s;
+	}
+	void show(){
+		System.out.println(n+" "+s);
+	}
+}
+public class Linkedlist_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			//클래스 설정 +
+	}
+
+}
